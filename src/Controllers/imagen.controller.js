0bd25002ba385801c3261840be3bc7f98a012621
Jumilo1998 <jimmy.granizo@espoch.edu.ec>
2023-11-22 09:28:48 -0500

    relaciones de creacion

--- src/Controllers/imagen.controller.js
+++ src/Controllers/imagen.controller.js
@@ -1,12 +1,17 @@
 //importar los modelas y sus relaciones
 import "../Database/relaciones.js";
+import { Articulo } from "../Models/articulo.js";
 import { Imagen } from "../Models/imagen.js"
 
 //CRUD basico para el modelo Imagen
 // Obtener la lista de imagenes 
 export const getAllImagenes = async (req, res) => {
     try {
-        const allImagenes =  await Imagen.findAll();
+        const allImagenes =  await Imagen.findAll({include: [{
+            model: Articulo,
+            attributes: ["id_articulo"],
+            required: true,
+        }]});
         res.json(allImagenes);
         console.log("Mostrando imagens resgistrados...");
     } catch (error) {
@@ -19,7 +24,12 @@ export const getImagenById = async (req, res) => {
     try {
       const { id_imagen } = req.params;
       const oneImagen = await Imagen.findOne({
-        where: { id_imagen}
+        where: { id_imagen},
+        include: [{
+          model: Articulo,
+          attributes: ["id_articulo"],
+          required: true,
+      }]
       });
       if (!oneImagen)
         return res.status(404).json({ message: "Imagen no registrado" });
@@ -34,18 +44,21 @@ export const createImagen  = async (req, res) => {
 
     // Espera recibir un paramentro "nombre" para crear el imagen
     const { url, cloudinary } = req.body;
+    const articulo = req.params.id_articulo; // Accede al id_articulo de los parámetros de la ruta
+
     try {
         // Creando un nuevo imagen con el metodo create
         const nuevoImagen =  await Imagen.create({
             url_imagen: url,
             id_imagen_cloudinary: cloudinary,
+            id_articulo: articulo
         });
         res.json(nuevoImagen);
         console.log("Nuevo imagen creado");
+        console.log("Nuevo imagen creado asociado al artículo ID:", articulo);
     } catch (error) {
         return res.status(500).json({message:error.message});
-    }
-   
+    }   
 };
 
 // Actualizar un imagen
@@ -54,7 +67,12 @@ export const updateImagen = async (req, res) => {
       const { id_imagen } = req.params;
       const { newUrl, newCloudinary } = req.body;
       const imagenActualizado = await Imagen.findOne({
-        where: { id_imagen }
+        where: { id_imagen },
+        include: [{
+          model: Articulo,
+          attributes: ["id_articulo"],
+          required: true,
+      }]
       });
       imagenActualizado.url_imagen = newUrl;
       imagenActualizado.id_imagen_cloudinary  = newCloudinary;
--- src/Routes/imagen.routes.js
+++ src/Routes/imagen.routes.js
@@ -6,7 +6,7 @@ const router =  Router ();
 
 //Rutas de conexion al controlador imagen
 router.get('/imagenes', getAllImagenes)
-router.post('/imagen', createImagen)
+router.post('/imagen/:id_articulo', createImagen)
 router.put('/imagen/:id_imagen', updateImagen)
 router.delete('/imagen/:id_imagen', deleteImagen)
 router.get('/imagen/:id_imagen', getImagenById)
