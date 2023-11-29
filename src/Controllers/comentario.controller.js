41891653c160246b6473d944b165a3624c9dbcc6
Jumilo98 <jgranizo98@hotmail.com>
2023-11-29 07:18:00 -0500

    uff

--- src/Controllers/comentario.controller.js
+++ src/Controllers/comentario.controller.js
@@ -5,16 +5,15 @@
 
   //CRUD basico para el modelo Comentarios
 
-  // Obtener la lista de comentarios 
-  export const getAllComentarios  = async (req, res) => {
+  // Obtener la lista de comentarios de producto
+  export const getAllComentariosPr  = async (req, res) => {
     const pagina = parseInt(req.query.pagina) || 1; // Obtiene el número de página desde la consulta, por defecto es 1
     const limite = 8;
     const offsetdinamic = (pagina - 1) * limite;
     try {
         const allComentarios =  await Comentario.findAndCountAll({
           include: [
-            { model: Producto },
-            { model: Punto }
+            { model: Producto }
           ],
           order: [
             ['id_comentario', 'DESC']
@@ -29,14 +28,37 @@
     }
   };
 
-  // Obtener un comentario en especifico 
-  export const getComentarioById = async (req, res) => {
+    // Obtener la lista de comentarios de punto
+    export const getAllComentariosP  = async (req, res) => {
+      const pagina = parseInt(req.query.pagina) || 1; // Obtiene el número de página desde la consulta, por defecto es 1
+      const limite = 8;
+      const offsetdinamic = (pagina - 1) * limite;
+      try {
+          const allComentarios =  await Comentario.findAndCountAll({
+            include: [
+              { model: Punto }
+            ],
+            order: [
+              ['id_comentario', 'DESC']
+            ],
+            limit: limite,
+            offset:offsetdinamic 
+          });
+          res.json(allComentarios);
+          console.log("Mostrando comentarios registrados...");
+      } catch (error) {
+          return res.status(500).json({message:error.message});
+      }
+    };
+
+
+  // Obtener un comentario en especifico de productos 
+  export const getComentarioPrById = async (req, res) => {
     try {
-      const { id_comentario } = req.body;
+      const { id_comentario } = req.params;
       const oneComentario = await Comentario.findByPk(id_comentario,{
         include: [
-          { model: Producto },
-          { model: Punto }
+          { model: Producto }
         ]
       });
       if (!oneComentario)
@@ -48,15 +70,51 @@
     }
   };
 
-  //Crear un comentario
-  export const createComentario = async (req, res) => {
+// Obtener un comentario en especifico de puntos
+export const getComentarioPById = async (req, res) => {
+  try {
+    const { id_comentario } = req.body;
+    const oneComentario = await Comentario.findByPk(id_comentario,{
+      include: [
+        { model: Punto }
+      ]
+    });
+    if (!oneComentario)
+      return res.status(404).json({ message: "Comentario no registrado" });
+    res.json(oneComentario);
+    
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+  //Crear un comentario para productos
+  export const createComentarioPr = async (req, res) => {
     // Espera recibir un paramentro "mensaje" para crear el comentario
-    const { mensaje, id_producto, id_punto } = req.body; // las foreign keys se les escribe tal y como son
+    const { id_producto } = req.params; 
+    const { mensaje } = req.body; // las foreign keys se les escribe tal y como son
     try {
         // Creando un nuevo objeto comentario con el metodo create
         const nuevoComentario =  await Comentario.create({
             mensaje_comentario: mensaje,     
-            id_producto,
+            id_producto
+        });
+        res.json(nuevoComentario);
+        console.log("Nuevo Comentario creado");
+    } catch (error) {
+        return res.status(500).json({message:error.message});
+    }
+  };
+
+  //Crear un comentario para productos
+  export const createComentarioP = async (req, res) => {
+    // Espera recibir un paramentro "mensaje" para crear el comentario
+    const { id_punto } = req.params; 
+    const { mensaje } = req.body; // las foreign keys se les escribe tal y como son
+    try {
+        // Creando un nuevo objeto comentario con el metodo create
+        const nuevoComentario =  await Comentario.create({
+            mensaje_comentario: mensaje,     
             id_punto
         });
         res.json(nuevoComentario);
@@ -66,10 +124,11 @@
     }
   };
 
-  // Actualizar un comentario
-  export const updateComentario = async (req, res) => {
+  // Actualizar un comentario de produto
+  export const updateComentarioPr = async (req, res) => {
+    const { id_comentario } = req.params; 
     try {
-      const { newMensaje, id_comentario } = req.body;
+      const { newMensaje } = req.body;
       const comentarioActualizado = await Comentario.findOne({
         where: { id_comentario }
       });
@@ -86,10 +145,48 @@
     }
   };
   
-  // Borrar un comentario
-  export const deleteComentario = async (req, res) => {
+  // Actualizar un comentario de punto
+  export const updateComentarioP = async (req, res) => {
+    const { id_comentario } = req.params; 
     try {
-      const { id_comentario } = req.body;
+      const { newMensaje } = req.body;
+      const comentarioActualizado = await Comentario.findOne({
+        where: { id_comentario }
+      });
+      // Validación
+      if(!comentarioActualizado) {
+       return res.status(404).json({mensaje: 'Comentario no encontrado'});
+      }
+      comentarioActualizado.mensaje_comentario = newMensaje;
+      if(await comentarioActualizado.save()) {
+        res.json({mensaje: 'Comentario actualizado'}) 
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  };
+
+  // Borrar un comentario de producto
+  export const deleteComentarioPr = async (req, res) => {
+    const { id_comentario } = req.params; 
+    try {
+      const comentarioEliminado = await Comentario.destroy({
+        where: {
+          id_comentario: id_comentario,
+        },
+      });
+      if(await comentarioEliminado.destroy()) {
+        res.json({mensaje: 'Comentario eliminado'}) 
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  };
+
+   // Borrar un comentario de punto
+   export const deleteComentarioP = async (req, res) => {
+    const { id_comentario } = req.params; 
+    try {
       const comentarioEliminado = await Comentario.destroy({
         where: {
           id_comentario: id_comentario,
--- src/Routes/comentario.routes.js
+++ src/Routes/comentario.routes.js
@@ -1,14 +1,23 @@
 import {Router} from 'express';
-import { getAllComentarios, getComentarioById , createComentario , updateComentario , deleteComentario } from '../Controllers/comentario.controller.js';
+import { getAllComentariosPr, getComentarioPrById , createComentarioPr , updateComentarioPr , deleteComentarioPr, 
+         getAllComentariosP, getComentarioPById , createComentarioP , updateComentarioP , deleteComentarioP
+} from '../Controllers/comentario.controller.js';
 
 const router =  Router ();
 
 
-//Rutas de conexion al controlador comentario
-router.get('/comentarios', getAllComentarios);
-router.post('/comentario/', createComentario);
-router.put('/comentario/', updateComentario); 
-router.delete('/comentario/', deleteComentario);
-router.get('/comentario/', getComentarioById); 
+//Rutas de conexion al controlador comentario Productos
+router.get('/productos/comentarios', getAllComentariosPr);
+router.post('/producto/comentario/:id', createComentarioPr);//id del producto
+router.put('/producto/comentario/:id', updateComentarioPr); //id del comentario
+router.delete('/producto/comentario/:id', deleteComentarioPr);//id del comentario
+router.get('/producto/comentario/:id', getComentarioPrById); //id del comentario
+
+//Rutas de conexion al controlador comentario Punto
+router.get('/puntos/comentarios', getAllComentariosP);
+router.post('/punto/comentario/', createComentarioP);//id del punto
+router.put('/punto/comentario/:id', updateComentarioP); //id del comentario
+router.delete('/punto/comentario:id/', deleteComentarioP);//id del comentario
+router.get('/punto/comentario/:id', getComentarioPById); // id del cometnario
 
 export default router
